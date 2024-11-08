f55158c8ffe08c43fa6ea2ee25f2883dd2049c57
Danny Bloomstine <dbloomstine@gmail.com>
2024-11-08 15:17:32 -0500

    Initial Commit

---
+++ SnoozeYouLose/SnoozeYouLoseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnoozeYouLoseApp.swift
+//  SnoozeYouLose
+//
+//  Created by Danny Bloomstine on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SnoozeYouLoseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnoozeYouLoseTests/SnoozeYouLoseTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnoozeYouLoseTests.swift
+//  SnoozeYouLoseTests
+//
+//  Created by Danny Bloomstine on 11/8/24.
+//
+
+import Testing
+@testable import SnoozeYouLose
+
+struct SnoozeYouLoseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
